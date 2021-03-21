d482adb9ca6b26f1f8ba1ce619fc3bc2daaf929d
lanbazz <62804907+lanbazz@users.noreply.github.com>
2021-03-21 20:58:54 +0100

    new login modal and register

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,4 +1,10 @@
+
 $( document ).ready(function() {
+    $('#signupForm').hide() ;
+    $('#userAcessModal').on('shown.bs.modal', function () {
+        $('#trigger_btn').trigger('focus')
+      }) ;
+      
     $('#discover').click(function(){
         $('#trending').removeClass('active') ;
         $('#following').removeClass('active') ;
@@ -14,4 +20,68 @@ $( document ).ready(function() {
         $('#discover').removeClass('active') ;
         $('#followers').addClass('active') ; 
     });
+
+
+    $('#signUpModalBtn').click(function(){
+        $('#signInModalBtn').removeClass('activeModalBtn') ; 
+        $('#loginForm').hide() ; 
+        $('#signUpModalBtn').addClass('activeModalBtn') ;  
+        $('#signupForm').show() ; 
+    });
+
+    $('#signInModalBtn').click(function(){
+        $('#signUpModalBtn').removeClass('activeModalBtn') ; 
+        $('#signupForm').hide() ; 
+        $('#signInModalBtn').addClass('activeModalBtn') ;  
+        $('#loginForm').show() ; 
+    });
+
+    // Icons Color change when value entered 
+    $('#signInUsernameInput').keyup(function(){
+        if ($('#signInUsernameInput').val().length != 0 ){
+            $('.logIn-user-container').css("color","rgba(238, 236, 236, 0.87)") ; 
+        } else if ( $('#signInUsernameInput').val().length == 0 ){
+           $('.logIn-user-container').css("color","#525e76") ; 
+        }
+    }) ; 
+
+    $('#signInPasswordInput').keyup(function(){
+        if ($('#signInPasswordInput').val().length != 0 ){
+            $('.logIn-pass-container').css("color","rgba(238, 236, 236, 0.87)") ; 
+        } else if ( $('#signInPasswordInput').val().length == 0 ){
+           $('.logIn-pass-container').css("color","#525e76") ; 
+        }
+    }) ; 
+
+    $('#signUpUsernameInput').keyup(function(){
+        if ($('#signUpUsernameInput').val().length != 0 ){
+            $('.signUp-user-container').css("color","rgba(238, 236, 236, 0.87)") ; 
+        } else if ( $('#signUpUsernameInput').val().length == 0 ){
+           $('.signUp-user-container').css("color","#525e76") ; 
+        }
+    }) ; 
+    $('#signUpPasswordInput').keyup(function(){
+        if ($('#signUpPasswordInput').val().length != 0 ){
+            $('.signUp-password-container').css("color","rgba(238, 236, 236, 0.87)") ; 
+        } else if ( $('#signUpPasswordInput').val().length == 0 ){
+           $('.signUp-password-container').css("color","#525e76") ; 
+        }
+    }) ; 
+
+    $('#signUpConfirmInput').keyup(function(){
+        if ($('#signUpConfirmInput').val().length != 0 ){
+            $('.signUp-confirm-container').css("color","rgba(238, 236, 236, 0.87)") ; 
+        } else if ( $('#signUpConfirmInput').val().length == 0 ){
+           $('.signUp-confirm-container').css("color","#525e76") ; 
+        }
+    }) ; 
+
+    $('#signUpEmailInput').keyup(function(){
+        if ($('#signUpEmailInput').val().length != 0 ){
+            $('.signUp-email-container').css("color","rgba(238, 236, 236, 0.87)") ; 
+        } else if ( $('#signUpEmailInput').val().length == 0 ){
+           $('.signUp-email-container').css("color","#525e76") ; 
+        }
+    }) ; 
+   
   });
