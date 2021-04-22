a2304e0ae99db07d47fe55cf4bcfedffe67e96df
lanbazz <62804907+lanbazz@users.noreply.github.com>
2021-04-22 15:11:05 +0100

    cake

---
+++ cake/js/main.js
@@ -0,0 +1,142 @@
+
+var AddCake1 = document.getElementById('cake1_add') ;
+var InCart1 = document.getElementById('cake1') ;
+
+AddCake1.addEventListener('click',function(){
+    InCart1.classList.toggle('in_cart') ;
+    
+    for(var i =0 ; i< InCart1.classList.length ; i++){
+        if ( InCart1.classList[i] == 'in_cart' ){
+            InCart1.innerHTML = "<img src='icons/in_cart.svg' >" ;
+        }
+        else 
+        {
+            InCart1.innerHTML = "<img src='icons/cart.svg' >" ;
+        }
+    }
+    
+});
+var AddCake2 = document.getElementById('cake2_add') ;
+var InCart2 = document.getElementById('cake2') ;
+
+AddCake2.addEventListener('click',function(){
+    InCart2.classList.toggle('in_cart') ;
+    
+    for(var i =0 ; i< InCart2.classList.length ; i++){
+        if ( InCart2.classList[i] == 'in_cart' ){
+            InCart2.innerHTML = "<img src='icons/in_cart.svg' >" ;
+        }
+        else 
+        {
+            InCart2.innerHTML = "<img src='icons/cart.svg' >" ;
+        }
+    }
+    
+});
+var AddCake3 = document.getElementById('cake3_add') ;
+var InCart3 = document.getElementById('cake3') ;
+
+AddCake3.addEventListener('click',function(){
+    InCart3.classList.toggle('in_cart') ;
+    
+    for(var i =0 ; i< InCart3.classList.length ; i++){
+        if ( InCart3.classList[i] == 'in_cart' ){
+            InCart3.innerHTML = "<img src='icons/in_cart.svg' >" ;
+        }
+        else 
+        {
+            InCart3.innerHTML = "<img src='icons/cart.svg' >" ;
+        }
+    }
+    
+});
+
+var AddCake4 = document.getElementById('cake4_add') ;
+var InCart4 = document.getElementById('cake4') ;
+
+AddCake4.addEventListener('click',function(){
+    InCart4.classList.toggle('in_cart') ;
+    
+    for(var i =0 ; i< InCart4.classList.length ; i++){
+        if ( InCart4.classList[i] == 'in_cart' ){
+            InCart4.innerHTML = "<img src='icons/in_cart.svg' >" ;
+        }
+        else 
+        {
+            InCart4.innerHTML = "<img src='icons/cart.svg' >" ;
+        }
+    }
+    
+});
+
+var AddCake5 = document.getElementById('cake5_add') ;
+var InCart5 = document.getElementById('cake5') ;
+
+AddCake5.addEventListener('click',function(){
+    InCart5.classList.toggle('in_cart') ;
+    
+    for(var i =0 ; i< InCart5.classList.length ; i++){
+        if ( InCart5.classList[i] == 'in_cart' ){
+            InCart5.innerHTML = "<img src='icons/in_cart.svg' >" ;
+        }
+        else 
+        {
+            InCart5.innerHTML = "<img src='icons/cart.svg' >" ;
+        }
+    }
+    
+});
+
+
+var AddCake6 = document.getElementById('cake6_add') ;
+var InCart6 = document.getElementById('cake6') ;
+
+AddCake6.addEventListener('click',function(){
+    InCart6.classList.toggle('in_cart') ;
+    
+    for(var i =0 ; i< InCart6.classList.length ; i++){
+        if ( InCart6.classList[i] == 'in_cart' ){
+            InCart6.innerHTML = "<img src='icons/in_cart.svg' >" ;
+        }
+        else 
+        {
+            InCart6.innerHTML = "<img src='icons/cart.svg' >" ;
+        }
+    }
+    
+});
+var AddCake7 = document.getElementById('cake7_add') ;
+var InCart7 = document.getElementById('cake7') ;
+
+AddCake7.addEventListener('click',function(){
+    InCart7.classList.toggle('in_cart') ;
+    
+    for(var i =0 ; i< InCart7.classList.length ; i++){
+        if ( InCart7.classList[i] == 'in_cart' ){
+            InCart7.innerHTML = "<img src='icons/in_cart.svg' >" ;
+        }
+        else 
+        {
+            InCart7.innerHTML = "<img src='icons/cart.svg' >" ;
+        }
+    }
+    
+});
+
+var AddCake8 = document.getElementById('cake8_add') ;
+var InCart8 = document.getElementById('cake8') ;
+
+AddCake8.addEventListener('click',function(){
+    InCart8.classList.toggle('in_cart') ;
+    
+    for(var i =0 ; i< InCart8.classList.length ; i++){
+        if ( InCart8.classList[i] == 'in_cart' ){
+            InCart8.innerHTML = "<img src='icons/in_cart.svg' >" ;
+        }
+        else 
+        {
+            InCart8.innerHTML = "<img src='icons/cart.svg' >" ;
+        }
+    }
+    
+});
